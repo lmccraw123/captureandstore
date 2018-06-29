51df408eee803333bb8f3074cc90769cc1a288c7
lmccraw123 <laurenmccraw@gmail.com>
2018-06-29 17:11:07 -0500

    imported file transfer

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +10,7 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, private camera: Camera) {
+  constructor(public navCtrl: NavController, private camera: Camera, private transfer: FileTransfer, private file: File) {
 
   }
 
@@ -28,4 +30,42 @@ export class HomePage {
    // Handle error
   });
 }
+
+const fileTransfer: FileTransferObject = this.transfer.create();
+
+// Upload a file:
+fileTransfer.upload(..).then(..).catch(..);
+
+// Download a file:
+fileTransfer.download(..).then(..).catch(..);
+
+// Abort active transfer:
+fileTransfer.abort();
+
+// full example
+upload() {
+  let options: FileUploadOptions = {
+     fileKey: 'file',
+     fileName: 'name.jpg',
+     headers: {}
+     .....
+  }
+
+  fileTransfer.upload('<file path>', '<api endpoint>', options)
+   .then((data) => {
+     // success
+   }, (err) => {
+     // error
+   })
+}
+*
+download() {
+  const url = 'http://www.example.com/file.pdf';
+  fileTransfer.download(url, this.file.dataDirectory + 'file.pdf').then((entry) => {
+    console.log('download complete: ' + entry.toURL());
+  }, (error) => {
+    // handle error
+  });
+}
+
 }
